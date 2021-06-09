59d890ef6cfe32e5465c9e2f7b187646e5e66f7d
Liam <liam.richens.11@gmail.com>
2021-06-09 10:12:55 +0100

    initial commit

---
+++ scrapbook_service_db/src/main/java/com/ScrapBook/scrapbook_service_db/ScrapbookServiceDbApplication.java
@@ -0,0 +1,13 @@
+package com.ScrapBook.scrapbook_service_db;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScrapbookServiceDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScrapbookServiceDbApplication.class, args);
+	}
+
+}
---
+++ scrapbook_service_db/src/main/java/com/ScrapBook/scrapbook_service_db/models/Message.java
@@ -0,0 +1,4 @@
+package com.ScrapBook.scrapbook_service_db.models;
+
+public class Message {
+}
