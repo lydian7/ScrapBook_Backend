6f173b9aaea055a099e268240ed7ab20608e6682
Liam <liam.richens.11@gmail.com>
2021-06-16 17:10:29 +0100

    dataloader changes

--- scrapbook_service_db/src/main/java/com/ScrapBook/scrapbook_service_db/components/DataLoader.java
+++ scrapbook_service_db/src/main/java/com/ScrapBook/scrapbook_service_db/components/DataLoader.java
@@ -40,9 +40,7 @@ public class DataLoader implements ApplicationRunner {
         roomRepository.save(room);
 
 
-
-
-        Room room2 = new Room("Memories", "Mary's ScrapBook", "123456", "mary@gmail.com");
+        Room room2 = new Room("CodeClan Tears", "where tears come true...", "123456", "mary@gmail.com");
 
         roomRepository.save(room2);
 
@@ -69,8 +67,9 @@ public class DataLoader implements ApplicationRunner {
         User user2 = new User("mary@gmail.com", "Mary", "01/06/1978", "123456");
         userRepository.save(user2);
 
-        User user3 = new User("liam@gmail.com", "Liam", "01/01/1990", "123456");
-        userRepository.save(user3);
+        User liam = new User("liam@gmail.com", "Liam", "01/01/1990", "123456");
+        liam.setProfilePicture("https://firebasestorage.googleapis.com/v0/b/scrapbook-245c8.appspot.com/o/profile-pics%2Fahmet-age-30.jpeg?alt=media&token=b04bc8b8-597c-4b95-a033-03527672e89d");
+        userRepository.save(liam);
 
         User user4 = new User("pooja@gmail.com", "Pooja", "01/01/1990", "123456");
         userRepository.save(user4);
@@ -78,94 +77,49 @@ public class DataLoader implements ApplicationRunner {
         Message message = new Message(user.getName(), LocalTime.now().toString(), "This is my first message!", room);
         messageRepository.save(message);
 
-        Post post = new Post(LocalTime.now().toString(),"My first time skydiving", "img/pic", room, user);
-        postRepository.save(post);
 
-        Post post1 = new Post(LocalTime.now().toString(), "Hello World", "img/pic", room2, user);
-        postRepository.save(post1);
 
-        Post post2 = new Post(LocalTime.now().toString(), "I love REACT", "img/pic", room3, user3);
+        Post post2 = new Post(LocalTime.now().toString(), "I love REACT", "img/pic", room3, liam);
         postRepository.save(post2);
+
+        Post post8 = new Post(LocalTime.now().toString(), "Climbing Holiday", "img/pic", room3, liam);
+        postRepository.save(post8);
 
 
         Post post3 = new Post(LocalTime.now().toString(), "NOT AN IMAGE", "img/pic", room4, user2);
-
         postRepository.save(post3);
 
-        Post post4 = new Post(LocalTime.now().toString(), "Visiting Edinburgh Castle", "img/pic", room3, user);
-        postRepository.save(post4);
-
-        Post post5 = new Post(LocalTime.now().toString(), "Day at the Beach", "img/pic", room4, user);
-        postRepository.save(post5);
-
-        Post post6 = new Post(LocalTime.now().toString(), "Party at Ally's", "img/pic", room3, user4);
-        postRepository.save(post6);
-
-        Post post7 = new Post(LocalTime.now().toString(), "Glasgow Road Trip", "img/pic", room3, user2);
+        Post post7 = new Post(LocalTime.now().toString(), "Glasgow Road Trip", "img/pic", room2, user2);
         postRepository.save(post7);
 
-        Post post8 = new Post(LocalTime.now().toString(), "Climbing Holiday", "img/pic", room3, user3);
-        postRepository.save(post8);
+
+        Post post6 = new Post(LocalTime.now().toString(), "Party at Ally's", "img/pic", room2, user4);
+        postRepository.save(post6);
 
         Post post9 = new Post(LocalTime.now().toString(), "Family Meal", "img/pic", room4, user4);
         postRepository.save(post9);
-
-        Post post10 = new Post(LocalTime.now().toString(), "Cutting the Cake", "img/pic", room5, user3);
-        postRepository.save(post10);
-
-        Post post11 = new Post(LocalTime.now().toString(), "Night Out", "img/pic", room6, user);
-        postRepository.save(post11);
-
-        Post post12 = new Post(LocalTime.now().toString(), "Getting up to Mischief", "img/pic", room6, user3);
-        postRepository.save(post12);
-
-        Post post13 = new Post(LocalTime.now().toString(), "Cohort Social", "img/pic", room7, user2);
-        postRepository.save(post13);
-
-        Post post14 = new Post(LocalTime.now().toString(), "Graduation", "img/pic", room7, user);
-        postRepository.save(post14);
-
-
-
 
 
 
 //        user.addRoom(room);
 //        userRepository.save(user);
 
-        room.addUser(user);
+        room.addUser(user4);
         roomRepository.save(room);
 
-        room2.addUser(user);
+
         room2.addUser(user2);
-        room2.addUser(user3);
+        room2.addUser(liam);
         room2.addUser(user4);
         roomRepository.save(room2);
 
-        room3.addUser(user2);
-        room3.addUser(user3);
+
+        room3.addUser(liam);
         roomRepository.save(room3);
 
-        room4.addUser(user);
         room4.addUser(user2);
-        room4.addUser(user3);
-        room4.addUser(user4);
         roomRepository.save(room4);
 
-        room5.addUser(user);
-        room5.addUser(user4);
-        roomRepository.save(room5);
-
-        room6.addUser(user);
-        room6.addUser(user2);
-        room6.addUser(user3);
-        room6.addUser(user4);
-        roomRepository.save(room6);
-
-        room7.addUser(user2);
-        room7.addUser(user3);
-        room7.addUser(user4);
-        roomRepository.save(room7);
 
     }
 }
